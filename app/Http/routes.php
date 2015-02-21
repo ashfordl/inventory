b7a5c37559ad3bc874a7a7c986415aabf4f107a9
ashfordl <luke.a.ashford@gmail.com>
2015-02-21 14:35:28 +0000

    Remove old seed route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,44 +28,7 @@ Route::get('item/{item?}', 'InventoryController@getItem');
 
 Route::post('item/{item?}', 'InventoryController@postItem');
 
-Route::get('seed', function()
-{
-    $user = new \Inventory\User();
-    $user->name = 'Quatermaster';
-    $user->password = Hash::make('password');
-    $user->email = 'quater@mast.er';
-    $user->save();
 
-    $category = new \Inventory\Category();
-    $category->name = 'MOSFETs';
-    $category->user()->associate($user);
-    $category->save();
-
-    $item = new \Inventory\Item();
-    $item->name = 'IRZ44N';
-    $item->user()->associate($user);
-    $item->category()->associate($category);
-    $item->save();
-
-    $project = new \Inventory\Project();
-    $project->name = 'Motor Driver Board';
-    $project->user()->associate($user);
-    $project->save();
-
-    $reference1 = new \Inventory\Reference();
-    $reference1->quantity = 6;
-    $reference1->item()->associate($item);
-    $reference1->project_id = null;
-    $reference1->save();
-
-    $reference2 = new \Inventory\Reference();
-    $reference2->quantity = 4;
-    $reference2->item()->associate($item);
-    $reference2->project()->associate($project);
-    $reference2->save();
-
-    echo "Data seeded successfully. 6 inserts, 0 updates, 0 deletions.";
-});
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
