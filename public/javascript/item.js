1745dd9869467ceb8817d539062f75a0177e7d7b
ashfordl <luke.a.ashford@gmail.com>
2015-03-09 18:08:40 +0000

    Refactor item submit fucntion
    This change makes the code more modular in function, and will make
    maintenance easier. Furthermore, the code is easier to read and
    understand.

--- public/javascript/item.js
+++ public/javascript/item.js
@@ -1,50 +1,82 @@
+var inventory = inventory || {};
+
+// Returns an array of all references to be submitted
+// Array format: [ {project_id, quantity}, ... ]
+inventory.findReferences = function() {
+    var references = [];
+
+    // For each reference, find the project id and quantity
+    $('.item-reference').each(function() {
+        var id = $(this).children('.reference-id').text();
+        var quantity = $(this).find('.reference-quantity').val();
+
+        references.push({ 'project_id': id, 'quantity': quantity });
+    });
+
+    return references;
+};
+
+// Creates the data object for the POST call
+inventory.createDataObject = function() {
+    var name = $('#item-name').val();
+    var categoryId = $('#item-category').val();
+    var references = inventory.findReferences();
+
+    var data = {
+        '_token': csrf
+        ,'id': itemId
+        ,'name': name
+        ,'category_id': categoryId
+        ,'references': references
+    };
+
+    return data;
+};
+
+// Upon successful call to server
+inventory.onSuccess = function(data) {
+    alert("Success!");
+
+    // If on new item page
+    if (window.location.href.indexOf("/item/new") > -1) {
+        // Redirect to /item/id
+        // PHP backend will echo the correct URL
+        window.location.href = data;
+    }
+};
+
+// Upon the server returning an error code
+inventory.onFailure = function(xhr, message, c) {
+    // If there is a validation error
+    if (xhr.status == 422) {
+        alert("Validation error: " + xhr.responseText);
+    }
+    // In all other cases
+    else {
+        alert("General error");
+    }
+
+    // Log the error to console for debug
+    console.log(xhr);
+};
+
+// Format JSON properly for submit
+inventory.submit = function(e) {
+    e.preventDefault();
+
+    // Retrieve item data
+    var data = inventory.createDataObject();
+
+    // Call server and handle response
+    $.post('item', data)
+        .done(inventory.onSuccess)
+        .fail(inventory.onFailure);
+};
+
 $(document).ready(function() {
 
     // Format JSON properly for submit
-    $('#form-item').submit(function(e) {
-        e.preventDefault();
-
-        var name = $('#item-name').val();
-        var categoryId = $('#item-category').val();
-        var references = [];
-
-        $('.item-reference').each(function() {
-            var id = $(this).children('.reference-id').text();
-
-            var quantity = $(this).find('.reference-quantity').val();
-            references.push({ 'project_id': id, 'quantity': quantity });
-        });
-
-        var data = {
-            '_token': csrf
-            ,'id': itemId
-            ,'name': name
-            ,'category_id': categoryId
-            ,'references': references
-        };
-
-        $.post('item', data)
-            .done(function(data) {
-                alert("Success!");
-
-                // If on new item page
-                if (window.location.href.indexOf("/item/new") > -1) {
-                    // Redirect to /item/id
-                    // PHP backend will echo the correct URL
-                    window.location.href = data;
-                }
-            })
-            .fail(function(xhr, message, c) {
-                if (xhr.status == 422) {
-                    alert("Validation error: " + xhr.responseText);
-                }
-                else {
-                    alert("General error");
-                }
-
-                console.log(xhr);
-            });
-    });
+    $('#form-item').submit(inventory.submit);
 
     // Add new reference row upon button press
     $('#reference-new-add').click(function(e) {
