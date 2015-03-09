2c0658caa9e9adcbbb86255e46956fa16885f420
ashfordl <luke.a.ashford@gmail.com>
2015-03-09 18:23:35 +0000

    Refactor add new reference method
    This change makes the code easier to read and maintain, by splitting the
    functionality up into smaller functions each performing one task only.

--- public/javascript/item.js
+++ public/javascript/item.js
@@ -73,41 +73,62 @@ inventory.submit = function(e) {
         .fail(inventory.onFailure);
 };
 
+inventory.findNewReferenceData = function() {
+    var data = {};
+
+    // Get selected option
+    data.selected = $('#reference-new-select option:selected');
+
+    // Get data of selected option
+    data.id = data.selected.val();
+    data.name = data.selected.text();
+    data.quantity = $('#reference-new-quantity').val();
+
+    return data;
+};
+
+inventory.displayNewReference = function(data) {
+    // Create new row element for table of references
+    var element =
+    '<tr class="item-reference">'
+        +'<td class="hidden reference-id"> {0} </td>'
+        +'<td> {1} </td>'
+        +'<td><input type="number" min="0" class="reference-quantity" value="{2}" /></td>'
+    +'</tr>';
+
+    // Format values into element
+    element = element.format(data.id, data.name, data.quantity);
+
+    // Append element
+    $('#item-references').append(element);
+};
+
+inventory.cleanupNewReferenceSelect = function(selected) {
+    // Remove selected option
+    selected.remove();
+
+    // If no other options, hide div
+    if ($('#reference-new-select option').length == 0) {
+        $('#reference-new-div').remove();
+    }
+};
+
+inventory.addNewReference = function(e) {
+    // Find data for new reference
+    var data = inventory.findNewReferenceData();
+
+    // Display the new reference
+    inventory.displayNewReference(data);
+
+    // Tidy up the new reference select
+    inventory.cleanupNewReferenceSelect(data.selected);
+};
+
 $(document).ready(function() {
 
     // Format JSON properly for submit
     $('#form-item').submit(inventory.submit);
 
     // Add new reference row upon button press
-    $('#reference-new-add').click(function(e) {
-        // Get selected option
-        var selected = $('#reference-new-select option:selected');
-
-        // Get data of selected option
-        var id = selected.val();
-        var name = selected.text();
-        var quantity = $('#reference-new-quantity').val();
-
-        // Create new row element for table of references
-        var element =
-        '<tr class="item-reference">'
-            +'<td class="hidden reference-id"> {0} </td>'
-            +'<td> {1} </td>'
-            +'<td><input type="number" min="0" class="reference-quantity" value="{2}" /></td>'
-        +'</tr>';
-
-        // Format values into element
-        element = element.format(id, name, quantity);
-
-        // Append element
-        $('#item-references').append(element);
-
-        // Remove selected option
-        selected.remove();
-
-        // If no other options, hide div
-        if ($('#reference-new-select option').length == 0) {
-            $('#reference-new-div').remove();
-        }
-    });
+    $('#reference-new-add').click(inventory.addNewReference);
 });
