ashfordl/inventory

commit 332362b78ac432f6cd8f25cbf785b53cf2d23d88

Refactor into separate controllers for Item and Project

resources/templates/inventory/item.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <h1>{{ $item->name or "New Item" }}</h1>
 
-    <form id="form-item" method="post" action="{{ action('InventoryController@postItem', isset($item) ? $item->id : -1) }}">
+    <form id="form-item" method="post" action="{{ route('item_post', isset($item) ? $item->id : -1) }}">
         <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
 
         Name
@@ -30,7 +30,6 @@
 
                 @foreach ($references as $reference)
                     <tr class="item-reference">
-                        <td class="hidden reference-id">{{ $reference->project->id or -1 }}</td>
                         <td>{{ $reference->project->name or "Spares" }}</td>
                         <td><input type="number" min="0" class="reference-quantity" value="{{ $reference->quantity or 0 }}" /></td>
                     </tr>

app/Http/Controllers/InventoryController.php
@@ -45,19 +45,6 @@ class InventoryController extends Controller {
         }
     }
 
-    public function getProject(Project $project)
-    {
-        $user = Auth::user();
-
-        $references = Item::selectAllForProject($project->id)
-                    ->get();
-
-        return view('inventory.project')
-            ->with(['user' => $user,
-                'project' => $project,
-                'references' => $references]);
-    }
-
     public function getSpares()
     {
         $user = Auth::user();
@@ -68,80 +55,5 @@ class InventoryController extends Controller {
         return view('inventory.spares')
             ->with('user', $user)
             ->with('items', $items);
-    }
-
-    public function getItem(Item $item = null)
-    {
-        $user = Auth::user();
-
-        // All categories, for drop-down category select
-        $categories = Category::all();
-
-        if (isset($item))
-        {
-            // All references (with positive quantity) involving this item
-            $references = Reference::allForItem($item->id);
-
-            // All projects without references to this item
-            $projects = Project::projectsWithoutItem($item->id);
-
-            // True if there are no spares for the item
-            $hasSpares = !Item::itemHasSpares($item->id);
-        }
-
-        return view('inventory.item')
-            ->with(['user' => $user,
-                'item' => $item,
-                'categories' => $categories,
-                'references' => $references,
-                'projects' => $projects,
-                'hasSpares' => $hasSpares
-            ]);
-    }
-
-    public function postItem(Request $request)
-    {
-        $this->validate($request, [
-            'id' => 'required | valueOrExists:-1,items,id',
-            'name' => 'required | max:255',
-            'category_id' => 'required | exists:categories,id',
-            'references.project_id' => 'required_with:references.quantity | exists:projects,id',
-            'references.quantity' => 'required_with:references.id | exists:projects,id'
-        ]);
-
-        // TODO existsForUser: new validator to restrict exists with a where clause
-        $data = \Input::all();
-
-        $item;
-        if ($data['id'] == -1)
-        {
-            $item = new Item;
-        }
-        else
-        {
-            $item = Item::find(['id' => $data['id']])->first();
-        }
-
-        $item->name = $data['name'];
-        $item->user_id = Auth::user()->id;
-        $item->category_id = $data['category_id'];
-        $item->save();
-
-
-        if (isset($data['references']))
-        {
-            foreach($data['references'] as $reference)
-            {
-                $proj_id = $reference['project_id'] == -1 ? null : $reference['project_id'];
-
-                $ref_model = Reference::firstOrNew(['item_id' => $item->id,
-                                            'project_id' => $proj_id]);
-                $ref_model->quantity = $reference['quantity'];
-                $ref_model->save();
-            }
-        }
-
-        // Echo URL to item page, so that user can be redirected away from /item/new
-        echo route("item_get", $item->id);
     }
 }

app/Http/Controllers/ItemController.php
@@ -1,0 +1,89 @@
+<?php namespace Inventory\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+use Inventory\Http\Controllers\Controller;
+use Inventory\Category;
+use Inventory\Item;
+use Inventory\Project;
+use Inventory\Reference;
+
+class ItemController extends Controller {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+	public function getItem(Item $item = null)
+    {
+        $user = Auth::user();
+
+        if (isset($item))
+        {
+            // All references (with positive quantity) involving this item
+            $references = Reference::allForItem($item->id);
+
+            // All projects without references to this item
+            $projects = Project::projectsWithoutItem($item->id);
+
+            // True if there are no spares for the item
+            $hasSpares = !Item::itemHasSpares($item->id);
+        }
+
+        return view('inventory.item')
+            ->with(['user' => $user,
+                'item' => $item,
+                'categories' => Category::all(),
+                'references' => $references,
+                'projects' => $projects,
+                'hasSpares' => $hasSpares
+            ]);
+    }
+
+    public function postItem(Request $request)
+    {
+        $this->validate($request, [
+            'id' => 'required | valueOrExists:-1,items,id',
+            'name' => 'required | max:255',
+            'category_id' => 'required | exists:categories,id',
+            'references.project_id' => 'required_with:references.quantity | exists:projects,id',
+            'references.quantity' => 'required_with:references.id | exists:projects,id'
+        ]);
+
+        // TODO existsForUser: new validator to restrict exists with a where clause
+        $data = \Input::all();
+
+        $item;
+        if ($data['id'] == -1)
+        {
+            $item = new Item;
+        }
+        else
+        {
+            $item = Item::find(['id' => $data['id']])->first();
+        }
+
+        $item->name = $data['name'];
+        $item->user_id = Auth::user()->id;
+        $item->category_id = $data['category_id'];
+        $item->save();
+
+
+        if (isset($data['references']))
+        {
+            foreach($data['references'] as $reference)
+            {
+                $proj_id = $reference['project_id'] == -1 ? null : $reference['project_id'];
+
+                $ref_model = Reference::firstOrNew(['item_id' => $item->id,
+                                            'project_id' => $proj_id]);
+                $ref_model->quantity = $reference['quantity'];
+                $ref_model->save();
+            }
+        }
+
+        // Echo URL to item page, so that user can be redirected away from /item/new
+        echo route("item_get", $item->id);
+    }
+}

app/Http/Controllers/ProjectController.php
@@ -1,0 +1,30 @@
+<?php namespace Inventory\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+use Inventory\Http\Controllers\Controller;
+use Inventory\Category;
+use Inventory\Item;
+use Inventory\Project;
+use Inventory\Reference;
+
+class ProjectController extends Controller {
+
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function getProject(Project $project)
+    {
+        $user = Auth::user();
+
+        $references = Item::selectAllForProject($project->id)
+                    ->get();
+
+        return view('inventory.project')
+            ->with(['user' => $user,
+                'project' => $project,
+                'references' => $references]);
+    }
+}

app/Http/routes.php
@@ -16,14 +16,13 @@ Route::model('project', 'Inventory\Project');
 Route::model('item', 'Inventory\Item');
 
 Route::get('/', ['as' => 'home', 'uses' => 'InventoryController@getIndex']);
-
-Route::get('project/{project}', ['as' => 'project', 'uses' => 'InventoryController@getProject']);
 Route::get('spares', ['as' => 'spares', 'uses' => 'InventoryController@getSpares']);
 
-Route::get('item/new', ['as' => 'item_new', 'uses' => 'InventoryController@getItem']);
-Route::get('item/{item}', ['as' => 'item_get', 'uses' => 'InventoryController@getItem']);
+Route::get('project/{project}', ['as' => 'project_get', 'uses' => 'ProjectController@getProject']);
 
-Route::post('item', ['as' => 'item_post', 'uses' => 'InventoryController@postItem']);
+Route::get('item/new', ['as' => 'item_new', 'uses' => 'ItemController@getItem']);
+Route::get('item/{item}', ['as' => 'item_get', 'uses' => 'ItemController@getItem']);
+Route::post('item', ['as' => 'item_post', 'uses' => 'ItemController@postItem']);
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',

resources/templates/inventory/inventories.blade.php
@@ -6,7 +6,7 @@
     <h3>Projects</h3>
         @if ($user->hasSpares())
             <ul>
-            <li><a href="{{ action('InventoryController@getSpares') }}">Spare components</a></li>
+            <li><a href="{{ route('spares') }}">Spare components</a></li>
         @elseif (empty($user->projects))
             <p>No projects</p>
         @else
@@ -14,7 +14,7 @@
         @endif
 
         @foreach($user->projects as $project)
-            <li><a href="{{ action('InventoryController@getProject', $project->id) }}">{{{ $project->name }}}</a></li>
+            <li><a href="{{ route('project_get', $project->id) }}">{{{ $project->name }}}</a></li>
         @endforeach
     </ul>
 
@@ -29,7 +29,7 @@
         </tr>
         @foreach($items as $item)
             <tr>
-                <td><a href="{{ action('InventoryController@getItem', $item->id) }}">{{{ $item->name }}}</a></td>
+                <td><a href="{{ route('item_get', $item->id) }}">{{{ $item->name }}}</a></td>
                 <td>{{{ $item->category }}}</td>
                 <td>{{{ $item->quantity }}}</td>
             </tr>
@@ -48,7 +48,7 @@
         </tr>
         @foreach($nostock as $item)
             <tr>
-                <td><a href="{{ action('InventoryController@getItem', $item->id) }}">{{{ $item->name }}}</a></td>
+                <td><a href="{{ route('item_get', $item->id) }}">{{{ $item->name }}}</a></td>
                 <td>{{{ $item->category }}}</td>
             </tr>
         @endforeach

resources/templates/inventory/project.blade.php
@@ -10,7 +10,7 @@
         </tr>
         @foreach($references as $reference)
             <tr>
-                <td><a href="{{ action('InventoryController@getItem', $reference->item->id) }}">{{{ $reference->item->name }}}</a></td>
+                <td><a href="{{ route('item_get', $reference->item->id) }}">{{{ $reference->item->name }}}</a></td>
                 <td>{{{ $reference->item->category->name }}}</td>
                 <td>{{{ $reference->quantity }}}</td>
             </tr>

resources/templates/inventory/spares.blade.php
@@ -10,7 +10,7 @@
         </tr>
         @foreach($items as $item)
             <tr>
-                <td><a href="{{ action('InventoryController@getItem', $item->id) }}">{{{ $item->name }}}</a></td>
+                <td><a href="{{ route('item_get', $item->id) }}">{{{ $item->name }}}</a></td>
                 <td>{{{ $item->category }}}</td>
                 <td>{{{ $item->quantity }}}</td>
             </tr>

commit c4790be30b1ecf11cc7f4ba84745468035b47ddd

Fix bug that caused item updates to fail
This bug caused a foreign key constraint error, as no project id value was
being provided, so the DB threw the error. This bug was introduced in
commit 332362b silently when the .reference-id div was removed, thus
causing no project-id value to be submitted to the PHP script.

resources/templates/inventory/item.blade.php
@@ -32,6 +32,7 @@
 
                 @foreach ($references as $reference)
                     <tr class="item-reference">
+                        <td class="hidden reference-id">{{ $reference->project->id or -1 }}</td>
                         <td>{{ $reference->project->name or "Spares" }}</td>
                         <td><input type="number" min="0" class="reference-quantity" value="{{ $reference->quantity or 0 }}" /></td>
                     </tr>